02664b822dec38d529fa7b07d27a7844f6fc4b2a
mertaksu28 <mertaksu28@outlook.com.tr>
2021-07-24 17:50:25 +0300

    edit

--- src/layouts/DashBoard.js
+++ src/layouts/DashBoard.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import JobAdvertisementList from '../pages/JobAdvertisementList'
-import EmployeeList from '../pages/EmployeeList'
-import EmployerList from '../pages/EmployerList'
-
+import React from "react";
+import JobAdvertisementList from "../pages/JobAdvertisementList";
+import EmployeeList from "../pages/EmployeeList";
+import EmployerList from "../pages/EmployerList";
+import { Grid, GridColumn, GridRow } from "semantic-ui-react";
 
 export default function DashBoard() {
-    return (
-        <div>
-            <JobAdvertisementList/>
-            <EmployeeList/>
-            <EmployerList/>
-        </div>
-    )
+  return (
+    <div>
+      <Grid>
+          <GridRow>
+              <GridColumn width="4">
+                  <JobAdvertisementList />
+              </GridColumn>
+              <GridColumn width="8">
+                  <EmployeeList />
+              </GridColumn>
+              <GridColumn>
+                  <EmployerList />
+              </GridColumn>
+        </GridRow>
+        
+        
+      </Grid>
+    </div>
+  );
 }
